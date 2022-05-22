4e16503ea727edbf7a384ef84bf07f37999413ec
Aftab Falak <aftabfalak956@gmail.com>
2022-05-22 18:48:32 +0500

    done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import './App.css';
-import ProductDetail from './components/ProductDetail/ProductDetail';
-import NavbarTop from './components/NavbarTop/NavbarTop'
 import NavbarBottom from './components/NavbarBottom/NavbarBottom'
 import { useState } from 'react';
 import MainDrawer from './components/MainDrawer/MainDrawer';
-import { red } from '@mui/material/colors';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
+import {
+  Routes,
+  Route,
+} from "react-router-dom";
+import ProductDetail from './components/ProductDetail/ProductDetail';
 
 function App() {
-
   const [open, setOpen] = useState({ drawerOpen: false })
-
-
   const setDrawerOpen = (isDrawerOpen) => {
     setOpen({
       drawerOpen: isDrawerOpen
@@ -22,22 +20,17 @@ function App() {
     setOpen((pstate) => ({ drawerOpen: !pstate.drawerOpen }))
   }
 
-  const theme = createTheme({
-    palette: {
-      primary: {
-        main: red[500],
-      },
-    },
-  });
 
   return (
     <div className="App">
-      <ThemeProvider theme={theme}>
-        <NavbarBottom toggleDrawer={toggleDrawer} />
+      <NavbarBottom toggleDrawer={toggleDrawer} />
+        <Routes>
+        {/* <Route path="/" element={<App ≥÷/>} /> */}
+        <Route path="/product" element={<ProductDetail />} />
+        </Routes>
         {/* <NavbarTop/> */}
         {/* <ProductDetail /> */}
-        {open.drawerOpen&&<MainDrawer open={open.drawerOpen} setDrawertoggle={setDrawerOpen} />}
-        </ThemeProvider>
+       <MainDrawer open={open.drawerOpen} setDrawertoggle={setDrawerOpen} />
     </div>
   );
 }
