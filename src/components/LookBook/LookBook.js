08862b9a44995ac8ab771f7f236c73efd041316c
Faisal Akram <78302283+mfaisalakram@users.noreply.github.com>
2022-08-01 18:29:39 +0500

    v

--- src/components/LookBook/LookBook.js
+++ src/components/LookBook/LookBook.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Slider from 'react-slick';
 import { Container, Row, Media, Col } from 'reactstrap';
 
 import './LookBook.css';
@@ -38,75 +39,162 @@ const LookBookDot = ({
 };
 
 const Lookbook = () => {
+  let settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+    ],
+  };
   return (
-    <div>
-      <section className="lookbook section-b-space ratio_square">
-        <Container>
-          <Row>
-            <Col md="6">
-              <div className="lookbook-block">
-                <div>
-                  <Media
-                    src={require('../../assets/images/Home/mainn.png')}
-                    className="img-fluid blur-up lazyload bg-img"
-                  />
-                </div>
-                <LookBookDot
-                  img={require('../../assets/images/pro3/ring.PNG')}
-                  brd={require('../../assets/images/VendorProfile/niche.jpeg')}
-                  num={'1'}
-                  title="Dolor ad hoc "
-                  details="details"
-                  classes="lookbook-dot"
-                  price="200$"
-                  link="#"
-                />
-                <LookBookDot
-                  img={require('../../assets/images/pro3/ring.PNG')}
-                  brd={require('../../assets/images/VendorProfile/niche.jpeg')}
-                  num={'2'}
-                  title="Dolor ad hoc "
-                  details="details"
-                  classes="lookbook-dot dot2"
-                  price="200$"
-                  link="#"
-                />
-              </div>
-            </Col>
-            <Col md="6">
-              <div className="lookbook-block">
-                <div>
-                  <Media
-                    src={require('../../assets/images/Home/mainn.png')}
-                    className="img-fluid blur-up lazyload bg-img"
-                  />
-                </div>
-                <LookBookDot
-                  img={require('../../assets/images/pro3/ring.PNG')}
-                  brd={require('../../assets/images/VendorProfile/niche.jpeg')}
-                  num={'1'}
-                  title="Dolor ad hoc "
-                  details="details"
-                  classes="lookbook-dot dot3"
-                  price="200$"
-                  link="#"
-                />
-                <LookBookDot
-                  img={require('../../assets/images/pro3/ring.PNG')}
-                  brd={require('../../assets/images/VendorProfile/niche.jpeg')}
-                  num={'2'}
-                  title="Dolor ad hoc "
-                  details="details"
-                  classes="lookbook-dot dot4"
-                  price="200$"
-                  link="#"
-                />
-              </div>
-            </Col>
-          </Row>
-        </Container>
-      </section>
-    </div>
+    <section className="lookbook section-b-space ratio_square border p-0 m-0">
+      <Slider {...settings}>
+        <div className="d-flex justify-content-center align-items-center">
+          <div className="lookbook-block mr-2">
+            <div>
+              <Media
+                src={require('../../assets/images/Home/mainn.png')}
+                className="img-fluid blur-up lazyload bg-img w-100"
+              />
+            </div>
+            <LookBookDot
+              img={require('../../assets/images/pro3/ring.PNG')}
+              brd={require('../../assets/images/VendorProfile/niche.jpeg')}
+              num={'1'}
+              title="Dolor ad hoc "
+              details="details"
+              classes="lookbook-dot"
+              price="200$"
+              link="#"
+            />
+            <LookBookDot
+              img={require('../../assets/images/pro3/ring.PNG')}
+              brd={require('../../assets/images/VendorProfile/niche.jpeg')}
+              num={'2'}
+              title="Dolor ad hoc "
+              details="details"
+              classes="lookbook-dot dot2"
+              price="200$"
+              link="#"
+            />
+          </div>
+
+          <div className="lookbook-block">
+            <div>
+              <Media
+                src={require('../../assets/images/Home/mainn.png')}
+                className="img-fluid blur-up lazyload bg-img w-100"
+              />
+            </div>
+            <LookBookDot
+              img={require('../../assets/images/pro3/ring.PNG')}
+              brd={require('../../assets/images/VendorProfile/niche.jpeg')}
+              num={'1'}
+              title="Dolor ad hoc "
+              details="details"
+              classes="lookbook-dot dot3"
+              price="200$"
+              link="#"
+            />
+            <LookBookDot
+              img={require('../../assets/images/pro3/ring.PNG')}
+              brd={require('../../assets/images/VendorProfile/niche.jpeg')}
+              num={'2'}
+              title="Dolor ad hoc "
+              details="details"
+              classes="lookbook-dot dot4"
+              price="200$"
+              link="#"
+            />
+          </div>
+        </div>
+        <div className="d-flex justify-content-center align-items-center">
+          <div className="lookbook-block mr-2">
+            <div>
+              <Media
+                src={require('../../assets/images/Home/mainn.png')}
+                className="img-fluid blur-up lazyload bg-img w-100"
+              />
+            </div>
+            <LookBookDot
+              img={require('../../assets/images/pro3/ring.PNG')}
+              brd={require('../../assets/images/VendorProfile/niche.jpeg')}
+              num={'1'}
+              title="Dolor ad hoc "
+              details="details"
+              classes="lookbook-dot"
+              price="200$"
+              link="#"
+            />
+            <LookBookDot
+              img={require('../../assets/images/pro3/ring.PNG')}
+              brd={require('../../assets/images/VendorProfile/niche.jpeg')}
+              num={'2'}
+              title="Dolor ad hoc "
+              details="details"
+              classes="lookbook-dot dot2"
+              price="200$"
+              link="#"
+            />
+          </div>
+
+          <div className="lookbook-block">
+            <div>
+              <Media
+                src={require('../../assets/images/Home/mainn.png')}
+                className="img-fluid blur-up lazyload bg-img w-100"
+              />
+            </div>
+            <LookBookDot
+              img={require('../../assets/images/pro3/ring.PNG')}
+              brd={require('../../assets/images/VendorProfile/niche.jpeg')}
+              num={'1'}
+              title="Dolor ad hoc "
+              details="details"
+              classes="lookbook-dot dot3"
+              price="200$"
+              link="#"
+            />
+            <LookBookDot
+              img={require('../../assets/images/pro3/ring.PNG')}
+              brd={require('../../assets/images/VendorProfile/niche.jpeg')}
+              num={'2'}
+              title="Dolor ad hoc "
+              details="details"
+              classes="lookbook-dot dot4"
+              price="200$"
+              link="#"
+            />
+          </div>
+        </div>
+      </Slider>
+    </section>
   );
 };
 
